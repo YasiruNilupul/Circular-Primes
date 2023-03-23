c84ff5960323b4a17a48bfffafbc9b41cf57122a
YasiruNilupul <wickramageyn.22@UOM.lk>
2023-03-23 09:10:20 +0530

    Update project Euler 35.py

--- project Euler 35.py
+++ project Euler 35.py
@@ -1,15 +1,15 @@
 #num='242342425'
 j=1
-k=0
-h=0
 primes=[]
-random_num=3
-for num in range(5):
+#random_num=0
+for num in range(12,13):
     #random_num="" 
     num=str(num)
     for k in range(len(num)-1):
      random_num=''
-     for i in range(0,len(num)):
+     
+     for i in range(1,len(num)):
+        #print("we")
         random_num= random_num+num[j]
         if j==len(num)-1 or j==0:
             j=0
@@ -17,26 +17,24 @@ for num in range(5):
             if k>0:
              j+=1
         else:
-            j+=1
+            j+=1                        #the code is running for 2 number numbers after changing j value
  #j=0
      if j==0 or j>1 :
-  #print(random_num)
-      if j>=2:
-         j+=1
-      else:
-          j=2
-    random_num=int(random_num)
-    def circu_num(numb):
-     h=0
-     for l in range(1,numb+1):
-       if numb%l==0:
+       print(random_num)
+     
+       h=0
+       random_num=int(random_num)
+       for l in range(1,random_num+1):
+        if random_num%l==0:
            h+=1
-     if h==2:
-        primes.append(numb)
-    circu_num(random_num)
-  
+       if h==2:
+         primes.append(random_num)
 
-
-
+      
+      
+       if j>=2:
+         j+=1
+       else:
+           j=2
 print(len(primes))
 #print(primes)
